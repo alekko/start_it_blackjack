d675865bf905e4b69f783855294d768ddd692694
arvids-dane <arvids.dane@gmail.com>
2019-10-09 17:05:50 +0300

    Izveidota klase BlackJack, kuru inicializē ielādējoties index.html failam, pieliktas divas podziņas pogu rindai un vēl spēles laukums, galds - gametable

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
-const table = new Table()
+class BlackJack {
+  constructor(id) {
+    this.galds = document.getElementById(id);
+   
+    this.divGameTable = document.createElement("div");
+    this.divGameTable.setAttribute("class", "gametable"); /**gametable - simple table with some decks and cards on it ;) */
+    this.galds.appendChild(this.divGameTable);
+
+    this.divButtonBar = document.createElement("div");
+    this.divButtonBar.setAttribute("class", "buttonbar");
+    let btnStartGame = document.createElement("button");
+    btnStartGame.innerHTML = "Sākt spēli!";
+    btnStartGame.onclick = (evt) => { 
+        this.startGame();
+    }
+
+    let btnTakeCard = document.createElement("button");
+    btnTakeCard.innerHTML = "Paņemt kārti!";
+    btnTakeCard.onclick = (evt) => { 
+        this.startGame();
+    }
+
+    this.divButtonBar.appendChild(btnStartGame);
+    this.divButtonBar.appendChild(btnTakeCard);
+
+    this.galds.appendChild(this.divButtonBar);
+  }
+
+startGame(){
+const ttable = new Table()
 const Players = []
  Players.push( new Player('Player One', 'player'))
  Players.push( new Player('Dealer', 'dealer'))
+
+console.log(Players[1])
 
 const deck1 = new Deck()
 deck1.build()
@@ -15,6 +46,10 @@ const decks = [
   deck1,
   deck2
 ]
+console.log(decks[0])
 
-table.addPlayers(Players)
+ttable.addPlayers(Players)
 // table.buildCardsFromDecks(decks)
+console.log(ttable)
+  }
+}
