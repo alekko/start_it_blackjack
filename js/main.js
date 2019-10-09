a8e029159c1ef07ca9b77c4e6db9699ffc0113c7
arvids-dane <arvids.dane@gmail.com>
2019-10-09 18:05:37 +0300

    labojumi, briest vajadzība pēc detalizētākas UML diagrammas ar visiem variantiem

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ class BlackJack {
     this.galds = document.getElementById(id);
    
     this.divGameTable = document.createElement("div");
-    this.divGameTable.setAttribute("class", "gametable"); /**gametable - simple table with some decks and cards on it ;) */
+    this.divGameTable.setAttribute("class", "gametable"); /**gametable - simple table with some decks and  players cards on it ;) */
     this.galds.appendChild(this.divGameTable);
 
     this.divButtonBar = document.createElement("div");
@@ -17,9 +17,13 @@ class BlackJack {
     let btnTakeCard = document.createElement("button");
     btnTakeCard.innerHTML = "Paņemt kārti!";
     btnTakeCard.onclick = (evt) => { 
-        this.startGame();
+        this.takeCard(); /**currentPlayer takeCard */
     }
-
+    let btnStay = document.createElement("button");
+    btnStay.innerHTML = "Izlaist gājienu!";
+    btnStay.onclick = (evt) => { 
+        this.passMove();/**Pass move, dealer now checking his situation, take card or pass move  */
+    }
     this.divButtonBar.appendChild(btnStartGame);
     this.divButtonBar.appendChild(btnTakeCard);
 
@@ -27,7 +31,7 @@ class BlackJack {
   }
 
 startGame(){
-const ttable = new Table()
+const gameTable = new Table()
 const players = []
  players.push( new Player('Player One', 'player'))
  players.push( new Player('Dealer', 'dealer'))
@@ -48,8 +52,8 @@ const decks = [
 ]
 console.log(decks[0])
 
-ttable.addPlayers(players)
-ttable.buildCardsFromDecks(decks)
-console.log(ttable)
+gameTable.addPlayers(players)
+gameTable.buildCardsFromDecks(decks)
+console.log(gameTable)
   }
 }
