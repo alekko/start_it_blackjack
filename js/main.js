fa49fc32e27ca971ea7a71e7094d763a6329fc08
Aleksandrs Kozlovs <aleksandrs.kozlovs@makit.lv>
2019-10-22 16:49:22 +0300

    wrongly merged code fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const MAX_POINTS = 21
-const CARD_NAMES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'A', 'A', 'A', 'A']
+const CARD_NAMES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 const CARD_SUITS = ['hearts', 'diamonds', 'spades', 'clubs']
 const CARD_ANIMATION_CLASS = 'fadeInLeft'
 
--- js/table.js
+++ js/table.js
@@ -6,8 +6,6 @@ class Table {
     this.currentPlayer = null
     this.currentCard = null
     this.bet = 10
-    this.cardsInDeck = deckCount * 52 - 4
-
   }
 
   player = () => this.players.find(player => player.type === 'player')
@@ -48,8 +46,6 @@ class Table {
       removeElementById('player-cash')
       document.getElementById('game').innerHTML += cashTemplate(this.getPlayerCash())
     }
-    this.buildCards()
-
   }
 
   startGame = () => {
@@ -64,23 +60,14 @@ class Table {
     this.addCardTo(this.dealer(), 'down')
   }
 
-
   takePlayerCard = () => {
-    this.player().addCard(this.takeCardFromDeck())
-    this.decCardsInDeck()
-    this.renderCardsAndPoints()
+    this.player().addCardTo(this.takeCardFromDeck())
     this.validatePlayerPoints()
-    
-    
   }
-
-  decCardsInDeck = () =>{
-    this.cardsInDeck = this.cardsInDeck - 1
 
   hit = () => {
     this.addCardTo(this.player())
     this.validatePlayerPoints()
-
   }
 
   stand = () => {
@@ -98,37 +85,10 @@ class Table {
     this.prepareTableForGame('end')
   }
 
-
-  downCardTemplate = () => `<div class='card card-down'></div>`
-  pointsTemplate = points => `<div class='points-block'><p>${points}</p></div>`
-  deckTemplate = cardsInDeck => `<div class='cardsindeck'>${cardsInDeck}</div>`
-  upCardTemplate = (suite, name, type) => `<div class='card ${suite} card-${type}'><p>${name}</p></div>`
-  notificationTemplate = message => `<div id='notification-block'><p>${message}</p></div>`
-  cashTemplate = cash => `<div class='cash'>CASH:<b>$${cash}</b></div>`
-  
-
-  renderCardsAndPoints = () => {
-    let playerDeck = document.getElementById('player')
-    let dealerDeck = document.getElementById('dealer')
-    let deck = document.getElementById('deck')
-    deck.innerHtml = playerDeck.innerHTML = dealerDeck.innerHTML = ''
-
-    this.player().cards.forEach(({ suite, name }) =>
-      playerDeck.innerHTML += this.upCardTemplate(suite, name)
-    )
-    playerDeck.innerHTML += this.pointsTemplate(this.player().getPoints())
-    playerDeck.innerHTML += this.cashTemplate(this.player().getCash())
-    deck.innerHTML += this.deckTemplate(this.cardsInDeck)
-    this.dealer().cards.forEach(({ suite, name, type }) =>
-      dealerDeck.innerHTML += type === 'down' ? this.downCardTemplate() : this.upCardTemplate(suite, name)
-    )
-    dealerDeck.innerHTML += this.pointsTemplate(this.dealer().getPoints())
-
   double = () => {
     this.addCardTo(this.player())
     this.validatePlayerPoints()
-    this.endTurn()
-
+    this.stand()
   }
 
   validatePlayerPoints = () => {
@@ -144,11 +104,11 @@ class Table {
     const dp = this.dealer().getPoints()
     let result = ''
 
-    if (pp > dp && pp <= MAX_POINTS || dp > MAX_POINTS){
+    if (pp > dp && pp <= MAX_POINTS || dp > MAX_POINTS) {
       result = 'win'
       this.updatePlayerCash(+20)
     }
-    else if (pp === dp){
+    else if (pp === dp) {
       result = 'draw'
       this.updatePlayerCash(+10)
     }
